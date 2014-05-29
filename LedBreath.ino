5774805876d3f40481a14dd8542ff46a73e24524
Oscar Chiu <oscar0529@gmail.com>
2014-05-30 01:58:56 +0800

    v1.0 ## first version

--- LedBreath.ino
+++ LedBreath.ino
@@ -1,40 +1,38 @@
+// #define DEBUG
+
+#ifdef DEBUG
+  #define logDebug(x, ...)  MyLog("[Debug][%s] "x, __FUNCTION__, ##__VA_ARGS__);
+#else
+  #define logDebug(x, ...) 
+#endif
 #define logInfo(x, ...)  MyLog("[%s] "x, __FUNCTION__, ##__VA_ARGS__);
 #define logWarning(x, ...)  MyLog("[Warning][%s] "x, __FUNCTION__, ##__VA_ARGS__);
 #define logError(x, ...)  MyLog("[Error][%s] "x, __FUNCTION__, ##__VA_ARGS__);
 
-static int g_counter = 0;
-const byte LED = 13;
-int angle = 0;
+const byte LED = 9;
+int angle = 180;
 
 extern void MyLog(const char *fmt, ... );
-extern int floatToStr(char *str, size_t size, double f, int precision);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
   Serial.begin(115200);
   Serial.println("Hello Arduino!");
-  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-//  logInfo("%s g_counter is %d, %s\n", __FUNCTION__, g_counter, g_counter%2 == 0 ? "even" : "odd");
-//  g_counter++;
-//  delay(1000);
-//  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);               // wait for a second
-//  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);               // wait for a second
-    
     float radius = angle*PI/180;
-    double out = sin(radius);
-    logInfo("angle=%d, out=%d.%d\n", angle, (int)out, (int)(out*100)%100);
+    byte brightness = (int)((cos(radius)+1) * 255) >> 1;
+    logInfo("angle=%d, brightness=%d\n", angle, brightness);
+    analogWrite(LED, brightness);
     angle++;
     if (angle == 360)
     {
       angle = 0;
     }
-    delay(100);
+    delay(30);
 }
 
 void MyLog(const char *fmt, ... ){
